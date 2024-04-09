b4a97a75402224dd6aa5fbb6ef011801e32b1c39
Alejandro Morales Rios <legorios@gmail.com>
2024-04-09 17:05:33 -0500

    Update currencyFormatter to remove decimal places

--- lib/currencyFormatter.ts
+++ lib/currencyFormatter.ts
@@ -11,11 +11,11 @@ export function currencyFormatter(numero: number, moneda: Currency): string {
 
   switch (moneda) {
     case 'COP':
-      return Number(numero).toLocaleString('es-CO', { style: 'currency', currency: 'COP' })
+      return Number(numero).toLocaleString('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0, maximumFractionDigits: 0 })
     case 'USD':
-      return Number(numero).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+      return Number(numero).toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0 })
     case 'EUR':
-      return Number(numero).toLocaleString('es-ES', { style: 'currency', currency: 'EUR' })
+      return Number(numero).toLocaleString('es-ES', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0, maximumFractionDigits: 0 })
     default:
       return '';
   }
