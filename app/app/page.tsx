b73bac8fd3bf76dc81c72cde4813c309646da0b1
Alejandro Morales Rios <legorios@gmail.com>
2024-05-25 19:03:38 -0500

    app page layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,6 +3,7 @@ import '@/app/styles/app-page.css'
 import { Metadata } from "next";
 import { ENV_VARS } from "@/global/env";
 import Image from "next/image";
+import { MyFooter } from "@/components/shared/footer/MyFooter";
 
 export const metadata: Metadata = {
   title: 'App móvil',
@@ -18,7 +19,7 @@ export default function App() {
   return (
     <main>
       <MyHeader />
-      <article className="px-6 sm:px-10 md:px-16 lg:px-20 xl:px-28 pt-20 md:pt-24 xl:pt- 28 flex flex-col mb-60">
+      <article className="px-6 sm:px-10 md:px-16 lg:px-20 xl:px-28 pt-20 md:pt-24 xl:pt- 28 flex flex-col mb-20">
         <section className="flex flex-col gap-16 md:gap-24 justify-center items-center h-[84vh] -translate-y-4">
           <div className="flex flex-col gap-6 items-center">
             <div className="rounded-3xl shadow-lg shadow-gray-400 px-8 py-6 w-fit">
@@ -37,57 +38,64 @@ export default function App() {
             <h2>desde el <span className="text-primary-600">EXTERIOR</span></h2>
           </div>
         </section>
-        <ul id="cards">
+        <ul id="cards" className="max-w-[1200px] self-center">
           <li className="card" id="card_1">
             <div className="card__content">
-              <div>
-                <h2>Card One</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <p><a href="#top" className="btn btn--accent">Read more</a></p>
+              <div className="bg-slate-100 grid grid-cols-2 justify-items-center rounded-lg mb-14">
+                <Image unoptimized src='/app/app-1.png' width={230} height={200} alt="App móvil" className="translate-y-14 w-36 sm:w-40 md:w-44 lg:w-48 xl:w-60"/>
+                <div className="flex flex-col justify-evenly items-center">
+                  <h2 className="text-center font-semibold text-xl md:text-2xl lg:text-3xl">Descarga nuestra<br />aplicación móvil</h2>
+                  <Image unoptimized src='/app/qr-app.png' width={230} height={230} alt="App QR" className="rounded-lg shadow-lg w-40 sm:w-44 md:w-48 lg:w-52 xl:w-56"/>
+                  <div className="flex gap-4">
+                    <Image unoptimized src='/app/google-play.png' width={150} height={50} alt="Google Play" className="w-24 md:w-28 lg:w-32 xl:w-36"/>
+                    <Image unoptimized src='/app/app-store.png' width={150} height={50} alt="App Store" className="w-24 md:w-28 lg:w-32 xl:w-36"/>
+                  </div>
+                </div>
               </div>
-              <figure>
-                <img src="/img-1.jpg" alt="Image description"/>
-              </figure>
             </div>
           </li>
           <li className="card" id="card_2">
-            <div className="card__content">
-              <div>
-                <h2>Card Two</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <p><a href="#top" className="btn btn--accent">Read more</a></p>
+            <div className="card__content shadow-2xl bg-white/70 backdrop-blur-lg">
+              <div className="grid grid-cols-2 justify-items-center rounded-lg">
+                <div className="flex flex-col justify-center items-center font-semibold gap-4 md:gap-6 lg:gap-10 py-10">
+                  <h3 className="text-lg md:text-xl lg:text-2xl text-center">Accede a un mapa dinámico de opciones inmobiliarias en toda Colombia, explora las mejores alternativas de inversión</h3>
+                  <h2 className="text-xl md:text-2xl lg:text-3xl text-center text-primary-600">¡Las mejores propiedades están al alcance de tu mano!</h2>
+                </div>
+                <div className=" w-full flex justify-center py-10">
+                  <Image unoptimized src='/app/app-2.png' width={230} height={612} alt="App móvil" className="w-auto h-[420px] sm:h-[440px] md:h-[460px] lg:h-[470px]"/>
+                </div>
               </div>
-              <figure>
-                <img src="/img-2.jpg" alt="Image description"/>
-              </figure>
             </div>
           </li>
           <li className="card" id="card_3">
             <div className="card__content">
-              <div>
-                <h2>Card Three</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <p><a href="#top" className="btn btn--accent">Read more</a></p>
+              <div className="bg-slate-200/80 backdrop-blur-lg grid grid-cols-2 justify-items-center shadow-2xl rounded-lg mb-14">
+                <div className=" w-full flex justify-center items-center">
+                  <Image unoptimized src='/app/app-3.png' width={230} height={200} alt="App móvil" className="sm:translate-y-14 w-36 sm:w-40 md:w-44 lg:w-64"/>
+                </div>
+                <div className="flex flex-col justify-center items-center font-semibold gap-4 md:gap-6 lg:gap-10 p-10">
+                  <h3 className="md:text-xl lg:text-2xl text-center">Descubre propiedades verificadas y seguras en Colombia. Inverclick te ofrece un acceso exclusivo a las mejores opciones.</h3>
+                  <h2 className="text-lg md:text-2xl lg:text-3xl text-center text-primary-600">¡Descarga nuestra app y comienza tu búsqueda!</h2>
+                </div>
               </div>
-              <figure>
-                <img src="/img-3.jpg" alt="Image description"/>
-              </figure>
             </div>
           </li>
           <li className="card" id="card_4">
-            <div className="card__content">
-              <div>
-                <h2>Card Four</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <p><a href="#top" className="btn btn--accent">Read more</a></p>
+            <div className="card__content shadow-2xl bg-white/80 backdrop-blur-lg">
+              <div className="grid grid-cols-2 justify-items-center rounded-lg p-1">
+                <div className="flex flex-col justify-center items-center font-semibold gap-4 md:gap-6 lg:gap-10 py-10">
+                  <h3 className=" md:text-xl lg:text-2xl text-center">Personaliza tu búsqueda usando filtros dinámicos. Nuestra tecnología facilita que tomes la mejor decisión.</h3>
+                  <h2 className="text-lg md:text-2xl lg:text-3xl text-center text-primary-600">¡Tu mejor inversión a un click de distancia!</h2>
+                </div>
+                <div className=" w-full flex justify-center items-center sm:p-20">
+                  <Image unoptimized src='/app/app-4.png' width={230} height={612} alt="App móvil" className="sm:rotate-[30deg] w-[180px] h-[250px] sm:w-auto sm:h-[440px] md:h-[460px] lg:h-[480px] xl:h-[512px]"/>
+                </div>
               </div>
-              <figure>
-                <img src="/img-2.jpg" alt="Image description"/>
-              </figure>
             </div>
           </li>
         </ul>
       </article>
+      <MyFooter />
     </main>
   );
 }
