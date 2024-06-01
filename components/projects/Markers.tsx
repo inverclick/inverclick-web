a773a0db07f8e6e925de16705cd567d489de934d
Juan Vanegas <eatteer@gmail.com>
2024-06-01 10:27:19 -0500

    feat: price on markers
    - Show price on markers
    - Limit price when currency is COP

--- components/projects/Markers.tsx
+++ components/projects/Markers.tsx
@@ -1,15 +1,20 @@
-import { Point } from "@/types/map";
+import { useCurrencyContext } from "@/contexts/CurrencyContext";
+import { currencyFormatter } from "@/lib/currencyFormatter";
+import { limitPrice } from "@/services/utils";
+import { IBLUEPRINT_POPULATED } from "@/types/blueprint";
 import { Marker, MarkerClusterer } from "@googlemaps/markerclusterer";
 import { AdvancedMarker, useMap } from "@vis.gl/react-google-maps";
 import { useEffect, useRef, useState } from "react";
 
-const DATA_URI = `data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjNjUxZWUzIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNDAgMjQwIiB3aWR0aD0iNTAiIGhlaWdodD0iNTAiPgo8Y2lyY2xlIGN4PSIxMjAiIGN5PSIxMjAiIG9wYWNpdHk9Ii42IiByPSI3MCIgLz4KPGNpcmNsZSBjeD0iMTIwIiBjeT0iMTIwIiBvcGFjaXR5PSIuMyIgcj0iOTAiIC8+Cjwvc3ZnPg==`;
+const DATA_URI = `data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjNjUxZWUzIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNDAgMjQwIiB3aWR0aD0iNTAiIGhlaWdodD0iNTAiPgo8Y2lyY2xlIGN4PSIxMjAiIGN5PSIxMjAiIG9wYWNpdHk9Ii45IiByPSI3MCIgLz4KPGNpcmNsZSBjeD0iMTIwIiBjeT0iMTIwIiBvcGFjaXR5PSIuMyIgcj0iOTAiIC8+Cjwvc3ZnPg==`;
 
 type MarkerProps = Readonly<{
-  points: Point[];
+  blueprints: IBLUEPRINT_POPULATED[];
 }>;
 
-export function Markers({ points }: MarkerProps) {
+export function Markers({ blueprints }: MarkerProps) {
+  const { convert, currency } = useCurrencyContext();
+
   const map = useMap();
   const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
   const clusterer = useRef<MarkerClusterer | null>(null);
@@ -31,7 +36,8 @@ export function Markers({ points }: MarkerProps) {
               label: {
                 text: String(cluster.count),
                 color: "#ffffff",
-                fontSize: "12px",
+                fontSize: "14px",
+                fontFamily: "Poppins",
                 fontWeight: "bold",
               },
             });
@@ -64,13 +70,24 @@ export function Markers({ points }: MarkerProps) {
 
   return (
     <>
-      {points.map((point) => {
+      {blueprints.map((blueprint) => {
         return (
           <AdvancedMarker
-            key={point.key}
-            position={{ lat: point.lat, lng: point.lng }}
-            ref={(marker) => setMarkerRef(marker, point.key)}
-          />
+            key={blueprint.project._id}
+            position={{
+              lat: blueprint.project.location.lat,
+              lng: blueprint.project.location.lng,
+            }}
+            ref={(marker) => setMarkerRef(marker, blueprint.project._id)}
+          >
+            <div className="bg-primary-700/90 p-2 rounded-full border-4 border-primary-100 border-opacity-50">
+              <span className="text-white font-medium">
+                {currency === "COP"
+                  ? limitPrice(blueprint.price, currency)
+                  : currencyFormatter(convert(blueprint.price), currency)}
+              </span>
+            </div>
+          </AdvancedMarker>
         );
       })}
     </>
--- services/utils.ts
+++ services/utils.ts
@@ -1,3 +1,4 @@
+import { Currency } from "@/contexts/CurrencyContext"
 import { IHOUSING_TYPE } from "@/types/project"
 
 const API = process.env.NEXT_PUBLIC_API 
@@ -66,4 +67,49 @@ export const getProjectCharacteristics = async (): Promise<{ success: boolean, m
       data: [],
     }
   }
+}
+
+/**
+ * Formats the price based on the currency.
+ * If the currency is "COP", the price is formatted as follows:
+ * - If the price is greater than or equal to 1,000,000, it is divided by 1,000,000 and displayed in millions.
+ * - If the price is greater than or equal to 1,000, it is divided by 1,000 and displayed in thousands.
+ * - Otherwise, the price is displayed as is.
+ * For other currencies, the price is displayed as is.
+ * @param price - The price to be formatted.
+ * @param currency - The currency of the price.
+ * @returns The formatted price with the currency symbol.
+ * 
+ * - Dividing by 1000000 leaves the millions.
+ * - Calling toFixed(1) leaves a single decimal place.
+ * - Calling replace removes the decimal places of value 0.
+ * 
+ * @example
+ * limitPrice(1000000, "COP") // "1M COP"
+ * limitPrice(100000, "COP") // "100K COP"
+ * limitPrice(1000, "COP") // "1K COP"
+ * limitPrice(100, "COP") // "100 COP"
+ * limitPrice(1000000, "USD") // "1000000 USD"
+ * limitPrice(100000, "USD") // "100000 USD"
+ * limitPrice(1000, "USD") // "1000 USD"
+ * limitPrice(100, "USD") // "100 USD"
+ * limitPrice(1000000, "EUR") // "1000000 EUR"
+ * limitPrice(100000, "EUR") // "100000 EUR"
+ * limitPrice(1000, "EUR") // "1000 EUR"
+ * limitPrice(100, "EUR") // "100 EUR"
+ */
+export const limitPrice = (price: number, currency: Currency) => {
+  if (currency === "COP") {
+    if (price >= 1_000_000) {
+      return (
+        (price / 1_000_000).toFixed(1).replace(/\.0$/, "") + "M " + currency
+      );
+    } else if (price >= 1000) {
+      return (price / 1000).toFixed(1).replace(/\.0$/, "") + "K " + currency;
+    } else {
+      return price + " " + currency;
+    }
+  }
+
+  return price + " " + currency;
 }
