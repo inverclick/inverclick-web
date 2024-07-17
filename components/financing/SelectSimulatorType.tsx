ff7b863e35f197d66d72e1d2cbc4c49b4812cda6
Juan Vanegas <eatteer@gmail.com>
2024-07-16 20:13:20 -0500

    feat: animate financing tabs

--- components/financing/SelectSimulatorType.tsx
+++ components/financing/SelectSimulatorType.tsx
@@ -1,21 +1,66 @@
-import React from 'react'
+import clsx from "clsx";
+import React, { useEffect, useRef } from "react";
 
 interface Props {
-  setSimulatorType: (type: 'VALOR' | 'CUOTA') => void
-  simulatorType: 'VALOR' | 'CUOTA'
+  setSimulatorType: (type: "VALOR" | "CUOTA") => void;
+  simulatorType: "VALOR" | "CUOTA";
 }
 
-export const SelectSimulatorType = ({setSimulatorType, simulatorType}: Props) => {
+export const SelectSimulatorType = ({
+  setSimulatorType,
+  simulatorType,
+}: Props) => {
+  const activeTabRef = useRef<HTMLButtonElement | null>(null);
+
+  const [width, setWidth] = React.useState(0);
+  const [offset, setOffset] = React.useState(0);
+
+  useEffect(() => {
+    if (activeTabRef.current) {
+      setWidth(activeTabRef.current.clientWidth);
+      setOffset(activeTabRef.current.offsetLeft);
+    }
+  }, [simulatorType, activeTabRef]);
+
   return (
-    <div className='border border-primary-600 w-fit rounded-full mb-12'>
-        <button 
-          onClick={() => setSimulatorType('VALOR')}
-          className={`${simulatorType === 'VALOR' ? 'bg-primary-600 text-white' : ''} px-6 py-2 rounded-full `}
-        >Valor de la vivienda</button>
-        <button 
-          onClick={() => setSimulatorType('CUOTA')}
-          className={`${simulatorType === 'CUOTA' ? 'bg-primary-600 text-white' : ''} px-6 py-2 rounded-full `}
-        >Cuota que quiero pagar</button>
-      </div>
-  )
-}
+    <div className="relative border border-primary-600 w-fit rounded-full mb-12 isolate">
+      <button
+        ref={(ref) => {
+          if (simulatorType === "VALOR") {
+            activeTabRef.current = ref;
+          }
+        }}
+        onClick={() => setSimulatorType("VALOR")}
+        className={clsx(
+          "px-6 py-2 rounded-full",
+          simulatorType === "VALOR" && activeTabRef.current && "text-white"
+        )}
+      >
+        Valor de la vivienda
+      </button>
+      <button
+        ref={(ref) => {
+          if (simulatorType === "CUOTA") {
+            activeTabRef.current = ref;
+          }
+        }}
+        onClick={() => setSimulatorType("CUOTA")}
+        className={clsx(
+          "px-6 py-2 rounded-full",
+          simulatorType === "CUOTA" && activeTabRef.current && "text-white"
+        )}
+      >
+        Cuota que quiero pagar
+      </button>
+      {activeTabRef.current && (
+        <div
+          className="absolute inset-0 h-full bg-primary-600 rounded-full transition-all -z-10"
+          style={{
+            width,
+            transform: `translateX(${offset}px)`,
+          }}
+        ></div>
+      )}
+    </div>
+  );
+};
