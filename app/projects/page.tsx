f5b048ed6d31ff06c5dfa11400c70b882dc4ee06
Juan Vanegas <eatteer@gmail.com>
2024-09-06 09:21:46 -0500

    refactor: set 35/65 - 50/50

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -45,12 +45,12 @@ export default async function Projects(props: any) {
       <ContactButton className="absolute right-4 bottom-4" />
       <section className="hidden lg:block">
         <DefaultResizablePanelGroup direction="horizontal">
-          <DefaultResizablePanel defaultSize={30}>
+          <DefaultResizablePanel defaultSize={35}>
             <MyMap2 blueprints={data} />
           </DefaultResizablePanel>
           <DefaultResizableHandle withHandle />
           <DefaultResizablePanel
-            defaultSize={70}
+            defaultSize={65}
             minSize={25}
             className="z-10 relative"
           >
