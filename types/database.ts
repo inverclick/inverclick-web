706c5ff92b0e5101f90b8d22f3a1e88e28d8e614
Juan Vanegas <eatteer@gmail.com>
2025-01-24 11:28:19 -0500

    feat: newsletter registration
    Allow users to register to newsletter

--- types/database.ts
+++ types/database.ts
@@ -470,6 +470,24 @@ export type Database = {
           },
         ];
       };
+      newsletter_users: {
+        Row: {
+          created_at: string;
+          email: string;
+          id: string;
+        };
+        Insert: {
+          created_at?: string;
+          email: string;
+          id?: string;
+        };
+        Update: {
+          created_at?: string;
+          email?: string;
+          id?: string;
+        };
+        Relationships: [];
+      };
       project_characteristics: {
         Row: {
           characteristic_id: string;
