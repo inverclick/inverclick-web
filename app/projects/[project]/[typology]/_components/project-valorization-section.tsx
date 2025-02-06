c4bfa9618c57ae5c245749a08495a9f62988ee2e
Juan Vanegas <eatteer@gmail.com>
2025-02-06 16:16:40 -0500

    refactor: project valorization section

--- app/projects/[project]/[typology]/_components/project-valorization-section.tsx
+++ app/projects/[project]/[typology]/_components/project-valorization-section.tsx
@@ -1,10 +1,10 @@
 import { Project } from "@/app/projects/[project]/[typology]/_services/get-project";
 import { useCurrencyContext } from "@/contexts/currency-context";
 import { formatCurrency } from "@/lib/format-currency";
+import { fixDecimals } from "@/lib/round-to-decimals";
 import { cn } from "@/lib/utils";
 import { Typography } from "@inverclick/inverclick-ui/typography";
 import { ComponentProps } from "react";
-import { fixDecimals } from "@/lib/round-to-decimals";
 
 export type ValorizaGraphSectionProps = {
   typology: Project["typologies"][0];
@@ -20,7 +20,7 @@ export const ProjectValorizationSection = ({
 }: ValorizaGraphSectionProps) => {
   const { currency, convert } = useCurrencyContext((s) => s);
 
-  const valorization = typology.price + typology.price * percentage;
+  const valorization = typology.price * (1 + percentage);
   const gain = typology.price * percentage;
 
   return (
@@ -28,54 +28,33 @@ export const ProjectValorizationSection = ({
       <Typography variant="h3" className="mb-12 lg:mb-24">
         Este proyecto potencia tu inversión y tu patrimonio*
       </Typography>
+
       <div className="mb-12 flex flex-col gap-4 lg:hidden">
-        <div className="flex items-start gap-1">
-          <div className="mt-1 aspect-square size-4 rounded-full bg-slate-500"></div>
-          <div className="flex flex-col">
-            <Typography>Valor actual:</Typography>
-            <Typography className="font-semibold">
-              {formatCurrency(convert(typology.price), currency)} {currency}
-            </Typography>
-          </div>
-        </div>
-        <div className="flex items-start gap-1">
-          <div className="mt-1 aspect-square size-4 rounded-full bg-teal-500"></div>
-          <div className="flex flex-col">
-            <Typography>Valor proyectado en {months} meses:</Typography>
-            <Typography className="font-semibold">
-              {formatCurrency(convert(valorization), currency)} {currency}
-            </Typography>
-          </div>
-        </div>
-        <div className="flex items-start gap-1">
-          <div className="mt-1 aspect-square size-4 rounded-full bg-green-500"></div>
-          <div className="flex flex-col">
-            <Typography>Podrías ganar en valoración:</Typography>
-            <Typography className="font-semibold">
-              {formatCurrency(convert(gain), currency)} {currency}{" "}
-              {`(${fixDecimals(percentage * 100)}%)`}
-            </Typography>
-          </div>
-        </div>
+        <InfoBlock
+          label="Valor actual:"
+          value={`${formatCurrency(convert(typology.price), currency)} ${currency}`}
+          color="bg-slate-500"
+        />
+        <InfoBlock
+          label={`Valor proyectado en ${months} meses:`}
+          value={`${formatCurrency(convert(valorization), currency)} ${currency}`}
+          color="bg-teal-500"
+        />
+        <InfoBlock
+          label="Podrías ganar en valoración:"
+          value={`${formatCurrency(convert(gain), currency)} ${currency} (${fixDecimals(percentage * 100)}%)`}
+          color="bg-green-500"
+        />
       </div>
+
       <div className="relative mb-4 flex h-56 w-full items-end gap-4 border-b lg:h-96">
-        <div
-          data-element="current-price-bar"
-          className="absolute left-[30%] w-10 -translate-x-1/2 md:w-20"
-          style={{
-            height: `calc(100% - (${percentage} * 100%))`,
-          }}
-        >
-          <div className="h-full w-full bg-slate-500"></div>
-          <div className="absolute bottom-full right-full hidden lg:block">
-            <Typography className="whitespace-nowrap text-right">
-              Valor actual:
-            </Typography>
-            <Typography variant="h4" className="whitespace-nowrap text-right">
-              {formatCurrency(convert(typology.price), currency)} {currency}
-            </Typography>
-          </div>
-        </div>
+        <PriceBar
+          label="Valor actual"
+          value={`${formatCurrency(convert(typology.price), currency)} ${currency}`}
+          color="bg-slate-500"
+          position="left-[30%]"
+          height={`calc(100% - (${percentage} * 100%))`}
+        />
         <div
           className="absolute right-[30%] hidden h-[0.5px] bg-black lg:left-[calc(30%+2.5rem)] lg:block"
           style={{
@@ -94,26 +73,70 @@ export const ProjectValorizationSection = ({
             </Typography>
           </div>
         </div>
-        <div
-          data-element="valorization-price-bar"
-          className="absolute left-[50%] h-full w-10 -translate-x-1/2 md:w-20"
-        >
-          <div className="h-full w-full bg-teal-500"></div>
-          <div className="absolute bottom-full right-full hidden lg:block">
-            <Typography className="whitespace-nowrap text-right">
-              Valor proyectado en {months} meses:
-            </Typography>
-            <Typography variant="h4" className="whitespace-nowrap text-right">
-              {formatCurrency(convert(valorization), currency)} {currency}
-            </Typography>
-          </div>
-        </div>
+        <PriceBar
+          label={`Valor proyectado en ${months} meses`}
+          value={`${formatCurrency(convert(valorization), currency)} ${currency}`}
+          color="bg-teal-500"
+          position="left-[50%] h-full"
+        />
         <div className="absolute bottom-full right-[30%] hidden h-[0.5px] bg-black lg:left-[calc(50%+2.5rem)] lg:block"></div>
       </div>
+
       <Typography>
         Estos valores son de referencia y representan cálculos estimados que
         pueden variar según las condiciones del mercado*
       </Typography>
     </section>
+  );
+};
+
+const InfoBlock = function ({
+  label,
+  value,
+  color,
+}: {
+  label: string;
+  value: string;
+  color: string;
+}) {
+  return (
+    <div className="flex items-start gap-1">
+      <div className={`mt-1 aspect-square size-4 rounded-full ${color}`}></div>
+      <div className="flex flex-col">
+        <Typography>{label}</Typography>
+        <Typography className="font-semibold">{value}</Typography>
+      </div>
+    </div>
+  );
+};
+
+const PriceBar = function ({
+  label,
+  value,
+  color,
+  position,
+  height,
+}: {
+  label: string;
+  value: string;
+  color: string;
+  position: string;
+  height?: string;
+}) {
+  return (
+    <div
+      className={`absolute ${position} w-10 -translate-x-1/2 md:w-20`}
+      style={{ height }}
+    >
+      <div className={`h-full w-full ${color}`}></div>
+      <div className="absolute bottom-full right-full hidden lg:block">
+        <Typography className="whitespace-nowrap text-right">
+          {label}
+        </Typography>
+        <Typography variant="h4" className="whitespace-nowrap text-right">
+          {value}
+        </Typography>
+      </div>
+    </div>
   );
 };
